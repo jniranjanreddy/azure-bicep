cfb344d8007eead82b4ff386fe5dc5f91cb004dc
Your Name <you@example.com>
2024-01-07 22:25:14 +0530

    wq

--- basics/appreg.sh
+++ basics/appreg.sh
@@ -1,3 +1,4 @@
-# az ad app create --display-name "MyApp" --reply-urls "http://localhost:3000" --homepage "http://localhost:3000"
+az ad app create --display-name "MyApp" --reply-urls "http://localhost:3000" --homepage "http://localhost:3000"
 
 
+az ad app create --display-name "MyApp" --reply-urls "http://localhost:3000" --homepage "http://localhost:3000" --resource-group jnr-iac
